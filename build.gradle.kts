97039f7b18d7257c631f1d727f4940acfa1983c1
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-09-29 17:44:51 +0200

    Make compatible with Gradle 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
